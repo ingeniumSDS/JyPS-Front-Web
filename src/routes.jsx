efbb6f14cbc82e9eea3c20544a25be91bb86cec1
EDUARDOlux <20243ds106@utez.edu.mx>
2026-03-23 21:50:30 -0600

    chore(rutas): agregar ruta para el módulo de Gestión de Departamentos

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,34 +2,38 @@ import { createBrowserRouter } from "react-router-dom";
 // Administrador
 import AdministradorLayout from "./pages/administrador/AdministradorLayout";
 import GestionUsuarios from "./pages/administrador/GestionUsuarios";
+import GestionDepartamentos from "./pages/administrador/GestionDepartamentos"; 
+
 // Generales
 import Login from "./pages/Login";
 import Recuperar from "./pages/Recuperar";
 
 export const router = createBrowserRouter([
     {
-    path: "/",
-    element: <Login />,
+      path: "/",
+      element: <Login />,
     },
     {
-    path: "/login",
-    element: <Login />,
+      path: "/login",
+      element: <Login />,
     },
     {
-    path: "/recuperar",
-    element: <Recuperar />,
+      path: "/recuperar",
+      element: <Recuperar />,
     },
-  //rutas protegidas mas adelante
+    // Rutas protegidas (panel de administrador)
     {
-    path: "/administrador",
-    element: <AdministradorLayout />,
-    children: [
-      { 
-        index: true,
-        element: <GestionUsuarios />
-      }
-      
-    ]
+      path: "/administrador",
+      element: <AdministradorLayout />,
+      children: [
+        { 
+          index: true,
+          element: <GestionUsuarios />
+        },
+        {
+          path: "departamentos", 
+          element: <GestionDepartamentos />
+        }
+      ]
     }
-  ]
-);
+]);
