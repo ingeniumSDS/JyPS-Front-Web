c9243efc015fce145fd0762db2869325653e6f35
EDUARDOlux <20243ds106@utez.edu.mx>
2026-04-02 20:38:48 -0600

    refactor: Separamos la logica de genstion y dejamos unicamente lo que engloba el login

--- src/hooks/useUsuarios.js
+++ src/hooks/useUsuarios.js
@@ -59,68 +59,11 @@ export const useUsuarios = () => {
         }
     };
 
-
-    //-----------------------------------------------------------------------
-    //POST - crear ususrio (Admin)
-    const crearUsuario = async (formData) => {
-        try {
-            //JSON
-            const payload = {
-                nombre: formData.nombre,
-                apellidoPaterno: formData.apellidoPaterno,
-                apellidoMaterno: formData.apellidoMaterno,
-                correo: formData.correo,
-                telefono: formData.telefono,
-                horaEntrada: `${formData.horaEntrada}:00`,
-                horaSalida: `${formData.horaSalida}:00`,
-                roles: formData.roles,
-                departamentoId: Number(formData.departamentoId) 
-            };
-            const respuesta = await request('/usuarios', 'POST', payload);
-            return { exito: true, data: respuesta };
-            
-        } catch (err) {
-            console.error("Error en useUsuarios:", err);
-            return { exito: false, mensaje: err.message };
-        }
-    };
-
-    //GET - obtener todos los ususarios (Admin)
-    const obtenerUsuarios = async () => {
-        try {
-            const respuesta = await request('/usuarios', 'GET');
-            
-            console.log("Datos puros del backend:", respuesta);
-
-            // Verificamos
-            const usuariosBackend = Array.isArray(respuesta) ? respuesta : (respuesta.data || []);
-
-            const usuariosFormateados = usuariosBackend.map(user => ({
-                id: user.id || Math.random().toString(),
-                nombreCompleto: user.nombreCompleto,
-                email: user.correo,
-                rol: user.roles && user.roles.length > 0 ? user.roles[0].toLowerCase() : 'sin_rol',
-                //Departamento  
-                departamento: user.departamento ? user.departamento.nombre : `Depto ${user.departamentoId}`,
-                telefono: user.telefono,
-                isActive: user.estatus !== undefined ? user.estatus : true 
-            }));
-
-            return { exito: true, data: usuariosFormateados };
-
-        } catch (err) {
-            console.error("Error en useUsuarios (obtener):", err);
-            return { exito: false, mensaje: err.message };
-        }
-    };
-
     return { 
         solicitarRecuperacion,
         verificarToken,
         restablecerContrasena,
         loginUsuario,
-        crearUsuario,
-        obtenerUsuarios, 
         isLoading 
     };
 };
