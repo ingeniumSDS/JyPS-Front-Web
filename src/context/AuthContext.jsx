77c3866f1bc71136f0d22f4dbebee13f6f90f2dc
EDUARDOlux <20243ds106@utez.edu.mx>
2026-04-04 01:23:05 -0600

    feat: Gestion y almacenamiento de datos del usuario, abstraccion de informacion del token

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,79 @@
+import { createContext, useContext, useState, useEffect } from 'react';
+import { useApi } from '../hooks/useApi'; 
+
+// CONTEXT 
+const AuthContext = createContext();
+
+// PROVIDER 
+export const AuthProvider = ({ children }) => {
+    
+    // ESTADOS 
+    const [user, setUser] = useState(null);
+    const [isLoadingSession, setIsLoadingSession] = useState(true);
+    const { request } = useApi();
+
+    //  LOGICA: DATOS 
+    const cargarDatosDelUsuario = async (tokenGuardado) => {
+        try {
+            const payload = JSON.parse(atob(tokenGuardado.split('.')[1]));
+            const usuarioId = payload.id; 
+
+            const datosDelBackend = await request(`/usuarios/${usuarioId}`, 'GET');
+            
+            setUser({
+                id: usuarioId,
+                nombre: datosDelBackend.nombreCompleto || "Sin Nombre",
+                rol: (datosDelBackend.roles && datosDelBackend.roles[0]) || "Usuario",
+                email: datosDelBackend.correo || "Sin Correo",
+                telefono: datosDelBackend.telefono || "Sin Teléfono"
+            });
+
+        } catch (error) {
+            console.error(" Falló la conexion con el Backend:", error);
+
+            setUser({
+                nombre: "Modo Fallback (Sin Backend)",
+                rol: "Administrador",
+                email: "error@conexion.com",
+                telefono: "000-0000"
+            });
+
+        } finally {
+            setIsLoadingSession(false);
+        }
+    };
+
+    // EFECTOS 
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            cargarDatosDelUsuario(token);
+        } else {
+            setIsLoadingSession(false);
+        }
+    }, []);
+
+    // HANDLERS: SESION 
+    const iniciarSesion = async (nuevoToken) => {
+        localStorage.setItem('token', nuevoToken);
+        setIsLoadingSession(true);
+        await cargarDatosDelUsuario(nuevoToken);
+    };
+
+    const cerrarSesion = () => {
+        localStorage.removeItem('token');
+        setUser(null);
+    };
+
+    // RENDER 
+    return (
+        <AuthContext.Provider value={{ user, isLoadingSession, iniciarSesion, cerrarSesion }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+// CUSTOM HOOK 
+export const useAuth = () => {
+    return useContext(AuthContext);
+};
