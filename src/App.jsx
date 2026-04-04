80a05b18337288f9927815880342d9dd77567682
EDUARDOlux <20243ds106@utez.edu.mx>
2026-04-04 00:34:40 -0600

    Refactor: Envolvemos el la app con el AuthContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
 import { Toaster } from 'sonner';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       <Toaster richColors position="top-right" />
       <RouterProvider router={router} />
-    </>
+    </AuthProvider>
   )
 }
 
